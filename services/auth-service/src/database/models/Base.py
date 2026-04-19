3e71644699d1b29472a19f0623bbe8ced5874aee
OmniSecura <OmniSecura@gmail.com>
2026-04-19 20:15:06 +0200

    User database table and database connection

---
+++ services/auth-service/src/database/models/Base.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from sqlalchemy import DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from sqlmodel import SQLModel
+
+class Base(SQLModel):
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime, server_default=func.now(), onupdate=func.now()
+    )
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
