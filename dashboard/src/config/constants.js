478013681527aee1927a022a1ab5931170ca1d08
OmniSecura <OmniSecura@gmail.com>
2026-05-15 16:58:24 +0200

    DAST-SCANS: frontend improvements + release tag (#13)

---
+++ dashboard/src/config/constants.js
@@ -0,0 +1,16 @@
+export const URLS = {
+  github:        'https://github.com/OmniSecura/CyberCore',
+  discord:       '#',
+  documentation: 'https://omnisecura.github.io/CyberCore/',
+  releases:      '#',
+  roadmap:       '#',
+  changelog:     '#',
+  sdks:          '#',
+  apiReference:  '#',
+  selfHosting:   '#',
+  contributing:  '#',
+  securityPolicy:'#',
+  vulnerability: 'mailto:security@cybercore.dev',
+}
+
+export const APP_VERSION = 'v0.0.3'
